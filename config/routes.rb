34fd846f9bca0700e6fe90dca7642d31da11ad2f
kazutoinao <kazutoinao@gmail.ci>
2021-11-03 09:09:18 +0000

    :tada: first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,46 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  devise_for :admins
-devise_for :admins, controllers: {
+ devise_for :admins, controllers: {
   sessions:      'admins/sessions',
   passwords:     'admins/passwords',
   registrations: 'admins/registrations'
-}
-devise_for :customers, controllers: {
-  sessions:      'customers/sessions',
-  passwords:     'customers/passwords',
-  registrations: 'customers/registrations'
-}
+ }
+ devise_for :customers, controllers: {
+  sessions:      'public/sessions',
+  passwords:     'public/passwords',
+  registrations: 'public/registrations'
+ }
+ root to: 'public/homes#top'
+ namespace :admin do
+    get 'homes/top' => 'homes#top'
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update,]
+    resources :order_details, only: [:update]
+ end
+
+  namespace :public do
+    get 'homes/top' => 'homes#top'
+    get 'homes/about' => 'homes#about'
+    resources :items, only: [:index, :show]
+    resources :customers, only: [:show, :edit, :update] do
+      collection do
+        get 'unsubscribe'
+        get 'withdraw'
+      end
+    end
+    resources :cart_items, only: [:index, :update, :create] do
+      collection do
+        get 'destroy'
+        get 'destroy_all'
+      end
+    end
+    resources :orders, only: [:new, :create, :index, :show] do
+      collection do
+        get 'confirm'
+        get 'complete'
+      end
+    end
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+  end
 end
